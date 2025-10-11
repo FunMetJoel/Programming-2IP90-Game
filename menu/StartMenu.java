93bf47f8c9e93d8cd1ae34e55126e2de7993ba52
FunMetJoel <machinebouwclub@gmail.com>
2025-10-11 16:53:44 +0200

    Further work on menu

--- menu/StartMenu.java
+++ menu/StartMenu.java
@@ -1,16 +1,10 @@
 package menu;
-import java.awt.Color;
+
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.awt.image.BufferedImage;
 
 import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,10 +12,16 @@ import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+/**
+ * The menu shown before the game starts.
+ */
 public class StartMenu extends JLayeredPane implements ComponentListener {
     BackgroundImage background;
     JPanel mainPanel;
 
+    /**
+     * Creates the menu object.
+     */
     public StartMenu() {
         this.setPreferredSize(new Dimension(800, 600));
         this.setDoubleBuffered(true);
@@ -29,32 +29,39 @@ public class StartMenu extends JLayeredPane implements ComponentListener {
         this.addComponentListener(this);
         
         this.background = new BackgroundImage();
-        this.background.setBounds(0, 0, this.getWidth(), this.getHeight());
         this.add(background, Integer.valueOf(0));
 
         this.mainPanel = new JPanel();
         this.mainPanel.setOpaque(true);
-        this.mainPanel.setBounds(this.getWidth()/10, this.getHeight()/10, this.getWidth() - 2*(this.getWidth()/10), this.getHeight() - 2*(this.getHeight()/10));
+        this.mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         this.add(mainPanel, Integer.valueOf(1));
 
 
-        // JLabel title = new JLabel("NAME"); 
-        // this.add(title, Integer.valueOf(1));
+        JLabel title = new JLabel("NAME"); 
+        title.setAlignmentX(CENTER_ALIGNMENT);
+        this.mainPanel.add(title, Integer.valueOf(1));
 
+        JTextField seedInput = new JTextField("Seed");
+        seedInput.setAlignmentX(CENTER_ALIGNMENT);
+        seedInput.setSize(100, 100);
+        this.mainPanel.add(seedInput, Integer.valueOf(1));
 
-        // JTextField seedInput = new JTextField("Seed");
-        // this.add(seedInput, Integer.valueOf(1));
-
-        // JButton newGameButton = new JButton("New Game");
-        // this.add(newGameButton, Integer.valueOf(1));
-
+        JButton newGameButton = new JButton("New Game");
+        newGameButton.setAlignmentX(CENTER_ALIGNMENT);
+        this.mainPanel.add(newGameButton, Integer.valueOf(1));
+        
+        this.componentResized(null);
         this.setVisible(true);
     }
 
     @Override
     public void componentResized(ComponentEvent e) {
         this.background.setBounds(0, 0, this.getWidth(), this.getHeight());
-        this.mainPanel.setBounds(this.getWidth()/10, this.getHeight()/10, this.getWidth() - 2*(this.getWidth()/10), this.getHeight() - 2*(this.getHeight()/10));
+        int padding = this.getWidth() / 10;
+        this.mainPanel.setBounds(
+            padding, padding, 
+            getWidth() - 2 * padding, getHeight() - 2 * padding
+        );
     }
 
     @Override
