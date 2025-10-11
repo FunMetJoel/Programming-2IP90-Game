a472cbca68236afbf8f101108ee61fd21f676c5d
MMac1ej <m.w.malek@student.tue.nl>
2025-10-11 11:46:28 +0200

    Made pseudo random permutations array generation

--- level/PermutationArrayGeneration.java
+++ level/PermutationArrayGeneration.java
@@ -3,7 +3,54 @@ package level;
 import java.util.ArrayList;
 
 public class PermutationArrayGeneration {
-    PseudoArrayGeneration randomNumbersArray = new PseudoArrayGeneration();
+    PseudoArrayGeneration generation = new PseudoArrayGeneration();
 
-    ArrayList<Integer> permutations = new ArrayList<Integer>();
+    int[] permutations = new int[256];
+
+    ArrayList<Float> randomNumbers = new PseudoArrayGeneration().getArray();
+
+    ArrayList<Integer> indicies = new ArrayList<Integer>();
+
+    public void fillInitialArray() {
+        for (int i = 0; i < 256; i++) {
+            permutations[i] = i;
+            // System.out.println(permutations.get(i));
+        }
+    }
+
+    public void createIndicies() {
+        for (int i = 0; i < randomNumbers.size(); i++) {
+            float number = randomNumbers.get(i) * 1000;
+            int index = Math.round(number) % 256;
+            indicies.add(index);
+        }
+    }
+
+    public void shuffleArray() {
+        for (int i = permutations.length - 1; i > 0; i -= 1) {
+            int index = indicies.get(i);
+            int tmp = permutations[i];
+            permutations[i] = permutations[index];
+            permutations[index] = tmp;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String arrayRepresentation = "";
+
+        for (int i = 0; i < permutations.length; i++) {
+            arrayRepresentation += permutations[i] + "\n";
+        }
+
+        return arrayRepresentation;
+    }
+
+    public static void main(String[] args) {
+        PermutationArrayGeneration generation = new PermutationArrayGeneration();
+        generation.createIndicies();
+        generation.fillInitialArray();
+        generation.shuffleArray();
+        System.out.println(generation);
+    }
 }
