bf4f00e33ac783bbe479f9baafc27919046d81da
MMac1ej <m.w.malek@student.tue.nl>
2025-10-21 14:07:17 +0200

    Made changes to fade and lerp. Added some javadocs

--- level/Fade.java
+++ level/Fade.java
@@ -1,16 +1,16 @@
 package level;
 
+/**Makes the transitions smoother, based on a function developed by Perlin.
+ * 
+ */
 public class Fade {
-    double x;
     
-    public Fade(double x) {
-        this.x = x;
-    }
-
-    public double calculateFade() {
-        // double value;
-        // value = ((6 * x - 15) * x + 10) * x * x * x;
-        // return value;
+    /**Calculates the fade value.
+     * 
+     * @param x value of the argument
+     * @return value of the fade function of the given argument
+     */
+    public static double calculateFade(double x) {
         return ((6 * x - 15) * x + 10) * x * x * x;
     }
 }
